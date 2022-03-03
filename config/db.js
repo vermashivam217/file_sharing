2c0d28fb1086d38cdaabd1a2c6c3ec50a2aa15ea
Shivam Soni <vermashivam217@gmail.com>
2022-03-03 18:31:49 +0530

    files

--- config/db.js
+++ config/db.js
@@ -1,19 +1,20 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+let url = 'mongodb://localhost:27017/inshare'
 function connectDB() {
     // Database connection
-    try {
-        mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
-        const connection = mongoose.connection;
-        connection.once('open', () => {
-            console.log('Database connected...');
-        })
-    } catch (error) {
-        console.log('Connection failed...')
-    }
+    mongoose.connect(url, { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => {
+        console.log('Database connected...');
+    }).catch((error) => {
+        console.log('Connection failed...', error)
+    })
 }
 
 
 module.exports = connectDB;
+
 
 

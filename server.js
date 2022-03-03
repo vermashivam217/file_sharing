44521bb3705fae5f45a27b58cc5b262d78329b5c
Shivam Soni <vermashivam217@gmail.com>
2022-03-03 18:31:49 +0530

    files

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-const express = require('express')
+const express = require('express');
 const app = express()
+const path = require('path')
 const port = process.env.port || 3000
-// const connectDB = require('./config/db')
-// connectDB();
+const connectDB = require('./config/db')
+connectDB();
+app.use(express.static('public'))
+app.use(express.json());
+//Template Engine 
 
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs')
+
+//Routes
+app.use('/api/files', require('./routes/file'));
+app.use('/files', require('./routes/show'));
+app.use('/files/download', require('./routes/donwload'))
 
 app.listen(port, () => {
     console.log(`File Sharing app listening on port ${port}!`)
